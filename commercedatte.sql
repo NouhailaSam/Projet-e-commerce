da86fe6105fdaf1bb56fd4c5773123394a401940
BAKADIRIMANE <aichaaima78@gmail.com>
2025-06-13 00:33:15 +0200

    Create commercedatte.sql

---
+++ commercedatte.sql
@@ -0,0 +1,339 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mar. 10 juin 2025 à 22:59
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `commercedatte`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `avis`
+--
+
+CREATE TABLE `avis` (
+  `avisId` int(11) NOT NULL,
+  `commentaire` text DEFAULT NULL,
+  `note` int(11) DEFAULT NULL CHECK (`note` between 0 and 5),
+  `clientId` int(11) DEFAULT NULL,
+  `produitId` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `client`
+--
+
+CREATE TABLE `client` (
+  `clientId` int(11) NOT NULL,
+  `nom` varchar(50) DEFAULT NULL,
+  `prénom` varchar(50) DEFAULT NULL,
+  `adresse` text DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commande`
+--
+
+CREATE TABLE `commande` (
+  `commandeId` int(11) NOT NULL,
+  `date_commande` date DEFAULT NULL,
+  `état` varchar(50) DEFAULT NULL,
+  `adresse_livraison` text DEFAULT NULL,
+  `clientId` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `détailcommande`
+--
+
+CREATE TABLE `détailcommande` (
+  `detailId` int(11) NOT NULL,
+  `quantité` int(11) DEFAULT NULL,
+  `commandeId` int(11) DEFAULT NULL,
+  `produitId` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `imageproduit`
+--
+
+CREATE TABLE `imageproduit` (
+  `imageId` int(11) NOT NULL,
+  `url_image` varchar(255) DEFAULT NULL,
+  `produitId` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `information`
+--
+
+CREATE TABLE `information` (
+  `informationId` int(11) NOT NULL,
+  `titre` varchar(100) DEFAULT NULL,
+  `contenu` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `langue`
+--
+
+CREATE TABLE `langue` (
+  `langueId` int(11) NOT NULL,
+  `nom_langue` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `panier`
+--
+
+CREATE TABLE `panier` (
+  `panierId` int(11) NOT NULL,
+  `date_création` date DEFAULT NULL,
+  `statut` varchar(50) DEFAULT NULL,
+  `clientId` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `produit`
+--
+
+CREATE TABLE `produit` (
+  `produitId` int(11) NOT NULL,
+  `désignation` varchar(100) DEFAULT NULL,
+  `prix` decimal(10,2) DEFAULT NULL,
+  `quantité_stock` int(11) DEFAULT NULL,
+  `date_mise_en_vente` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `promotion`
+--
+
+CREATE TABLE `promotion` (
+  `promotionId` int(11) NOT NULL,
+  `taux_promo` decimal(5,2) DEFAULT NULL,
+  `date_debut` date DEFAULT NULL,
+  `date_fin` date DEFAULT NULL,
+  `produitId` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `avis`
+--
+ALTER TABLE `avis`
+  ADD PRIMARY KEY (`avisId`),
+  ADD KEY `clientId` (`clientId`),
+  ADD KEY `produitId` (`produitId`);
+
+--
+-- Index pour la table `client`
+--
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`clientId`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Index pour la table `commande`
+--
+ALTER TABLE `commande`
+  ADD PRIMARY KEY (`commandeId`),
+  ADD KEY `clientId` (`clientId`);
+
+--
+-- Index pour la table `détailcommande`
+--
+ALTER TABLE `détailcommande`
+  ADD PRIMARY KEY (`detailId`),
+  ADD KEY `commandeId` (`commandeId`),
+  ADD KEY `produitId` (`produitId`);
+
+--
+-- Index pour la table `imageproduit`
+--
+ALTER TABLE `imageproduit`
+  ADD PRIMARY KEY (`imageId`),
+  ADD KEY `produitId` (`produitId`);
+
+--
+-- Index pour la table `information`
+--
+ALTER TABLE `information`
+  ADD PRIMARY KEY (`informationId`);
+
+--
+-- Index pour la table `langue`
+--
+ALTER TABLE `langue`
+  ADD PRIMARY KEY (`langueId`),
+  ADD UNIQUE KEY `nom_langue` (`nom_langue`);
+
+--
+-- Index pour la table `panier`
+--
+ALTER TABLE `panier`
+  ADD PRIMARY KEY (`panierId`),
+  ADD KEY `clientId` (`clientId`);
+
+--
+-- Index pour la table `produit`
+--
+ALTER TABLE `produit`
+  ADD PRIMARY KEY (`produitId`);
+
+--
+-- Index pour la table `promotion`
+--
+ALTER TABLE `promotion`
+  ADD PRIMARY KEY (`promotionId`),
+  ADD KEY `produitId` (`produitId`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `avis`
+--
+ALTER TABLE `avis`
+  MODIFY `avisId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `client`
+--
+ALTER TABLE `client`
+  MODIFY `clientId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `commande`
+--
+ALTER TABLE `commande`
+  MODIFY `commandeId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `détailcommande`
+--
+ALTER TABLE `détailcommande`
+  MODIFY `detailId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `imageproduit`
+--
+ALTER TABLE `imageproduit`
+  MODIFY `imageId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `information`
+--
+ALTER TABLE `information`
+  MODIFY `informationId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `langue`
+--
+ALTER TABLE `langue`
+  MODIFY `langueId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `panier`
+--
+ALTER TABLE `panier`
+  MODIFY `panierId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `produit`
+--
+ALTER TABLE `produit`
+  MODIFY `produitId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `promotion`
+--
+ALTER TABLE `promotion`
+  MODIFY `promotionId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `avis`
+--
+ALTER TABLE `avis`
+  ADD CONSTRAINT `avis_ibfk_1` FOREIGN KEY (`clientId`) REFERENCES `client` (`clientId`),
+  ADD CONSTRAINT `avis_ibfk_2` FOREIGN KEY (`produitId`) REFERENCES `produit` (`produitId`);
+
+--
+-- Contraintes pour la table `commande`
+--
+ALTER TABLE `commande`
+  ADD CONSTRAINT `commande_ibfk_1` FOREIGN KEY (`clientId`) REFERENCES `client` (`clientId`);
+
+--
+-- Contraintes pour la table `détailcommande`
+--
+ALTER TABLE `détailcommande`
+  ADD CONSTRAINT `détailcommande_ibfk_1` FOREIGN KEY (`commandeId`) REFERENCES `commande` (`commandeId`),
+  ADD CONSTRAINT `détailcommande_ibfk_2` FOREIGN KEY (`produitId`) REFERENCES `produit` (`produitId`);
+
+--
+-- Contraintes pour la table `imageproduit`
+--
+ALTER TABLE `imageproduit`
+  ADD CONSTRAINT `imageproduit_ibfk_1` FOREIGN KEY (`produitId`) REFERENCES `produit` (`produitId`);
+
+--
+-- Contraintes pour la table `panier`
+--
+ALTER TABLE `panier`
+  ADD CONSTRAINT `panier_ibfk_1` FOREIGN KEY (`clientId`) REFERENCES `client` (`clientId`);
+
+--
+-- Contraintes pour la table `promotion`
+--
+ALTER TABLE `promotion`
+  ADD CONSTRAINT `promotion_ibfk_1` FOREIGN KEY (`produitId`) REFERENCES `produit` (`produitId`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
